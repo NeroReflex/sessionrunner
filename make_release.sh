6c7d5cacf02b9ecab441a3fe5a8eb910cb1e0a6e
benato.denis96@gmail.com <benato.denis96@gmail.com>
2025-11-03 17:04:43 +0100

    Fix: make_release.sh script

--- make_release.sh
+++ make_release.sh
@@ -3,11 +3,10 @@
 set -e
 
 new_version=${1}
-version=$(grep -oP 'version\s*=\s*"\K[0-9]+\.[0-9]+\.[0-9]+' sessionrunner/Cargo.toml | head -n 1)
+version=$(grep -oP 'version\s*=\s*"\K[0-9]+\.[0-9]+\.[0-9]+' Cargo.toml | head -n 1)
 echo "Replacing version $version with $new_version"
 find . -name "*.toml"  -type f -exec sed -i "s/\"$version\"/\"$new_version\"/g" {} +
-cd sessionrunner && cargo bitbake && cd ..
-cd sessionexec && cargo bitbake && cd ..
+cargo bitbake
 git add .
 git commit -m "Bump version to $new_version"
 git tag $new_version
